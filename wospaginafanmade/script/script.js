9cb2a85bcb9878b8802b3ba9c791b46b2d220102
Samuel Barba Anton <95043338+Samucraft346@users.noreply.github.com>
2024-06-23 17:21:48 +0200

    Add files via upload

---
+++ wospaginafanmade/script/script.js
@@ -0,0 +1,67 @@
+const btnAbrirModal =
+    document.querySelector(".btn-abrir-modal");
+const btnCerrarModal =
+    document.querySelector(".btn-cerrar-modal");
+const modal =
+    document.querySelector("#modal");
+const btnAbrirModal2 =
+    document.querySelector(".btn-abrir-modal2");
+const btnCerrarModal2 =
+    document.querySelector(".btn-cerrar-modal2");
+const modal2 =
+    document.querySelector("#modal2");
+const btnAbrirModal3 =
+    document.querySelector(".btn-abrir-modal3");
+const btnCerrarModal3 =
+    document.querySelector(".btn-cerrar-modal3");
+const modal3 =
+    document.querySelector("#modal3");
+const btnAbrirModal4 =
+    document.querySelector(".btn-abrir-modal4");
+const btnCerrarModal4 =
+    document.querySelector(".btn-cerrar-modal4");
+const modal4 =
+    document.querySelector("#modal4");
+const btnAbrirModal5 =
+    document.querySelector(".btn-abrir-modal5");
+const btnCerrarModal5 =
+    document.querySelector(".btn-cerrar-modal5");
+const modal5 =
+    document.querySelector("#modal5");
+
+btnAbrirModal.addEventListener("click",() =>{
+    modal.showModal();
+})
+
+btnCerrarModal.addEventListener("click",() =>{
+    modal.close();
+})
+btnAbrirModal2.addEventListener("click",() =>{
+    modal2.showModal();
+})
+
+btnCerrarModal2.addEventListener("click",() =>{
+    modal2.close();
+})
+btnAbrirModal3.addEventListener("click",() =>{
+    modal3.showModal();
+})
+
+btnCerrarModal3.addEventListener("click",() =>{
+    modal3.close();
+})
+btnAbrirModal4.addEventListener("click",() =>{
+    modal4.showModal();
+})
+
+btnCerrarModal4.addEventListener("click",() =>{
+    modal4.close();
+})
+btnAbrirModal5.addEventListener("click",() =>{
+    modal5.showModal();
+})
+
+btnCerrarModal5.addEventListener("click",() =>{
+    modal5.close();
+})
+
